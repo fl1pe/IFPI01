c7b4115ddea4ece49c675a5b6d43b20fb353721e
Felipe Bezerra <felipbsousa036@gmail.com>
2021-05-30 09:51:34 -0300

    Initial commit

---
+++ desconto_hotel.py
@@ -0,0 +1,10 @@
+
+tipo = int(input('INFORME SUA CATEGORIA: \n1-Estudante\n2-Trabalhador da indústria\n3-Sócio do clube de viagem\n4-Aposentado\nNº '))
+if tipo== 1:
+    print('VOCÊ GANHOU R$50 DE DESCONTO!')
+elif tipo== 2:
+    print('VOCÊ GANHOU R$30 DE DESCONTO!')
+elif tipo== 3:
+    print('VOCÊ GANHOU R$80 DE DESCONTO!')
+elif tipo== 4:
+    print('VOCÊ GANHOU R$100 DE DESCONTO!')
