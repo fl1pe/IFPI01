b62d0c7cc36c55b844d02572576a3770818fe054
Felipe Bezerra <felipbsousa036@gmail.com>
2021-05-30 09:51:34 -0300

    Initial commit

---
+++ LER A TEMPERATURA E MOSTRAR A SITUAÇAO.py
@@ -0,0 +1,13 @@
+##   QUESTAO 6.0 ###
+
+
+a1 = float(input('INFORME SUA TEMPERATURA EM ºC: '))
+b1 = a1
+if b1 < 36.5:
+    print('\tHipotermia!')
+elif b1 >= 36.5 and b1 < 37:
+    print('\tNormal!')
+elif b1 > 37 and b1 < 38:
+    print('\tEstado febril!')
+elif b1 > 38:
+    print('\tFebre!')
