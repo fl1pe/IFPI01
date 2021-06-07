61b6ffc06e8f4cfc8c9cdeebb2722282882c74b7
Felipe Bezerra <felipbsousa036@gmail.com>
2021-06-06 22:13:52 -0300

    a

--- exercicio01.py
+++ exercicio01.py
@@ -1,5 +1,6 @@
 #               CALCULAR MÉDIA
 n1 = float(input('digite sua 1º nota '))
 n2 = float(input('Digite sua 2º nota '))
-m =float(n1 + n2) / 2
+n3 = float(input('Digite sua 3º nota '))
+m =float(n1 + n2 + n3) / 3
 print(f'Sua média é {m:.2f}!')
